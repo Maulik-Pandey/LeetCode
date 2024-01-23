b5dfce810845a4edcf33a069bb28642f6396f143
Maulik-Pandey <maulik.pandey_cs.ccv21@gla.ac.in>
2024-01-23 10:59:51 +0530

    Committed

---
+++ src/postOrder145.java
@@ -0,0 +1,12 @@
+//public class postOrder145 (TreeNode root) {
+//    if(root ==null)
+//
+//    {
+//        return;
+//    }
+//
+//    postOrder145(root.left);
+//
+//    postOrder145(root.right);
+//System.out.println(root.data+" ");
+//}
